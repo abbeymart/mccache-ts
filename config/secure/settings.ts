19f35e5bc6b2e35272a01c4c8926374ec17e13e8
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2020-11-29 17:38:30 -0500

    refactored functions

---
+++ config/secure/settings.ts
@@ -0,0 +1,10 @@
+/**
+ * @Author: abbeymart | Abi Akindele | @Created: 2020-07-09
+ * @Company: Copyright 2020 Abi Akindele  | mConnect.biz
+ * @License: All Rights Reserved | LICENSE.md
+ * @Description: mc: bookmark socket.io
+ */
+
+export const constants = {
+    keyCode: 'mcconnect_20200320',
+}
